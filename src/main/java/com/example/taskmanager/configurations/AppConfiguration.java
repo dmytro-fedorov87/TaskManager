ecf61e78de326164d01ca518c78c326c707d82ee
Dmytro Fedorov <mitya.fedorov1987@gmail.com>
2023-10-12 16:13:49 +0300

    12.10.23
    Create AppConfiration, application.properties and added dependency in pom.xml

---
+++ src/main/java/com/example/taskmanager/configurations/AppConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.taskmanager.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@PropertySource("classpath:application.properties")
+public class AppConfiguration {
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Bean
+    @Scope("prototype")
+    public SimpleMailMessage massageTemplate() {
+        SimpleMailMessage simpleMessage = new SimpleMailMessage();
+        simpleMessage.setSubject("Task");
+        simpleMessage.setText("Hello. Your Task is: \n\n [%s] %s");
+        simpleMessage.setFrom(fromEmail);
+        return simpleMessage;
+    }
+}
