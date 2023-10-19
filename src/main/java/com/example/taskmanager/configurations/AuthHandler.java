dde5827e5739cd7bd8656b0989ab2d7ae17be4f1
Dmytro Fedorov <mitya.fedorov1987@gmail.com>
2023-10-19 17:19:23 +0300

    authentificationSuccessHandler 2

--- src/main/java/com/example/taskmanager/configurations/AuthHandler.java
+++ src/main/java/com/example/taskmanager/configurations/AuthHandler.java
@@ -4,14 +4,16 @@ import com.example.taskmanager.dto.AccountDTO;
 import com.example.taskmanager.services.AccountService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.annotations.Comment;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Map;
-
+@Component
 public class AuthHandler implements AuthenticationSuccessHandler {
     private final AccountService accountService;
 
