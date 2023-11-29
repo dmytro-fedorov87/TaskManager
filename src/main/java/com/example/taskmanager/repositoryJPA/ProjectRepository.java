bde6325e5ab669df8d1aeea112f9d759248fbe21
Dmytro Fedorov <mitya.fedorov1987@gmail.com>
2023-11-29 13:15:56 +0200

    11

--- src/main/java/com/example/taskmanager/repositoryJPA/ProjectRepository.java
+++ src/main/java/com/example/taskmanager/repositoryJPA/ProjectRepository.java
@@ -4,7 +4,6 @@ import com.example.taskmanager.model.Condition;
 import com.example.taskmanager.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
